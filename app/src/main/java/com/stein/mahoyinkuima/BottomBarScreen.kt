8dddd50ce059c4a6e0764dbb4b062ccd9193ecc7
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-09-30 01:26:37 +0900

    chore: tidy up code

--- app/src/main/java/com/stein/mahoyinkuima/BottomBarScreen.kt
+++ app/src/main/java/com/stein/mahoyinkuima/BottomBarScreen.kt
@@ -6,8 +6,8 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 
 sealed class BottomBarScreen(val route: String, val title: String, val icon: ImageVector) {
-    object Home : BottomBarScreen(route = "HOME", title = "HOME", icon = Icons.Default.Home)
+    data object Home : BottomBarScreen(route = "HOME", title = "HOME", icon = Icons.Default.Home)
 
-    object Settings :
+    data object Settings :
             BottomBarScreen(route = "SETTINGS", title = "SETTINGS", icon = Icons.Default.Settings)
 }
